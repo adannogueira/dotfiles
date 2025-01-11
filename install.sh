f8b671ee6a273cdc9ec810294f3896603e192d55
Adan Nogueira <adan.nogueira@gmail.com>
2025-01-11 17:40:53 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Install Packages
-sudo apt update -y
-sudo apt install bat -y
+sudo apt-get update -y
+sudo apt-get install bat -y
 
 # Stow everything
 mkdir ~/.config
@@ -12,6 +12,6 @@ stow --target ~/.config . -v
 # Setup Zsh
 command -v zsh | sudo tee -a /etc/shells
 sudo chsh -s $(which zsh) $USER
-antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
+antibody bundle < ~/dotfiles/.zsh_plugins.txt > ~/.zsh_plugins.sh
 exec zsh
 
