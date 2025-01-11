04df81e932ec7bea9fe0a8b99c4b5c4f32e95bd5
Adan Nogueira <adan.nogueira@gmail.com>
2025-01-11 18:07:59 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,11 +7,13 @@ curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
 
 # Stow everything
 cd ~/dotfiles
-stow --target ~/ . -v
+stow git
+stow nvim
+stow p10k
+stow zsh
 
 # Setup Zsh
 command -v zsh | sudo tee -a /etc/shells
 sudo chsh -s $(which zsh) $USER
 antibody bundle < ~/dotfiles/zsh/.zsh_plugins.txt > ~/.zsh_plugins.sh
 exec zsh
-
